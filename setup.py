5452f743215aedb78df7cbf97295c1aff8d758de
Sukhsimran Singh <sukhsimransingh304@gmail.com>
2025-12-19 15:01:10 +0530

    setup.py and requirements.txt added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+
+setup(
+name='End to End ML Project',
+version='0.0.1',
+author='Sukhsimran Singh',
+author_email='sukhsimransingh304@gmail.com',
+packages=find_packages(),
+install_requires=['pandas','numpy','seaborn']
+
+)
