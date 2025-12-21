f9141dbf26706ac4fb67e695a492cf3faaa0a0e2
Sukhsimran Singh <sukhsimransingh304@gmail.com>
2025-12-21 17:49:06 +0530

    updated prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -1,42 +1,40 @@
 import os
-import sys 
+import sys
 import numpy as np
 import pandas as pd
+import joblib
 from src.exception import CustomException
-import pickle 
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
-def save_object(file_path,obj):
-    try:
-        dir_path=os.path.dirname(file_path)
-        os.makedirs(dir_path,exist_ok=True)
-        with open(file_path ,"wb") as file_obj:
-            pickle.dump(obj,file_obj)
 
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        joblib.dump(obj, file_path)
     except Exception as e:
-        raise CustomException (e,sys)
-    
-def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+        raise CustomException(e, sys)
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
-        for i in range(len(list(models))):
+        for i in range(len(models)):
             model = list(models.values())[i]
-            para=param[list(models.keys())[i]]
+            para = param[list(models.keys())[i]]
 
-            gs= GridSearchCV(model,para,cv=3)
-            gs.fit(X_train,y_train)
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
 
             model.set_params(**gs.best_params_)
-            model.fit(X_train,y_train)
-            #model.fit(X_train, y_train)  # Train model
+            model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
-
             y_test_pred = model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
-
             test_model_score = r2_score(y_test, y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
@@ -45,10 +43,10 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 
     except Exception as e:
         raise CustomException(e, sys)
-    
+
+
 def load_object(file_path):
     try:
-        with open(file_path,"rb") as file_obj:
-            return pickle.load(file_obj)
+        return joblib.load(file_path)
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
