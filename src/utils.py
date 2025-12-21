0ed422819362887de630134ffe35f2b3bc4216c1
Sukhsimran Singh <sukhsimransingh304@gmail.com>
2025-12-21 16:40:30 +0530

    utils

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ import sys
 import numpy as np
 import pandas as pd
 from src.exception import CustomException
-import dill 
+import pickle 
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 def save_object(file_path,obj):
@@ -11,7 +11,7 @@ def save_object(file_path,obj):
         dir_path=os.path.dirname(file_path)
         os.makedirs(dir_path,exist_ok=True)
         with open(file_path ,"wb") as file_obj:
-            dill.dump(obj,file_obj)
+            pickle.dump(obj,file_obj)
 
     except Exception as e:
         raise CustomException (e,sys)
@@ -49,6 +49,6 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 def load_object(file_path):
     try:
         with open(file_path,"rb") as file_obj:
-            return dill.load(file_obj)
+            return pickle.load(file_obj)
     except Exception as e:
         raise CustomException(e,sys)
